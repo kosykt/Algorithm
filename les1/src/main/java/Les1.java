456d294fdc1e71933a62583e42175ff32bf5b2f9
kosykt <kosvasl09@gmail.com>
2021-04-13 20:16:41 +0900

    WH1

--- les1/src/main/java/Les1.java
+++
@@ -1,2 +0,0 @@
-public class Les1 {
-}
---
+++ les1/src/main/java/WH1.java
@@ -0,0 +1,56 @@
+public class WH1 {
+
+    /*
+    Task 1.1
+        Приготовления чая:
+        1. Вскипятить чайник.
+        2. Взять кружку и заварку.
+        3. Налить заварку, затем воду.
+        4. Добавить сахар.
+        5. Перемешать.
+
+    Task 1.2
+        Алгоритмы:
+        1. Алгоритм поиска
+        2. Алгоритм сортировки
+        3. Алгиритм удиления
+        4. Алгоритм сляния
+
+        Cтруктуры данных:
+        1. Массив (Array)
+        2. Стек (Stack)
+        3. Очередь (Queue)
+        4. Связный список (Linked List)
+        5. Дерево (Tree)
+        6. Граф (Graph)
+        7. Префиксное дерево (Trie)
+        8. Хэш-Таблица (Hash Table)
+     */
+
+    public static void main(String[] args) {
+
+//  Task 1.3
+        int i = 101;
+        String s = "Hello";
+        MyClass myClass = new MyClass(010, "World");
+
+    }
+}
+
+class MyClass{
+    private int num;
+    private String txt;
+
+    public MyClass(int num, String txt) {
+        this.num = num;
+        this.txt = txt;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public String getTxt() {
+        return txt;
+    }
+}
