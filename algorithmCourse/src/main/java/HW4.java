cba2d5ca4b1cedd15d92175f72e4c2a3560b2e30
kosykt <kosvasl09@gmail.com>
2021-04-17 22:33:22 +0900

    task 4.5-1

--- algorithmCourse/src/main/java/HW4.java
+++ algorithmCourse/src/main/java/HW4.java
@@ -60,8 +60,8 @@ public class HW4 {
         //Task 4.5
         MyStackList4 msl4 = new MyStackList4();
 
-        long t5 = System.nanoTime();
-        
+        long t5_1 = System.nanoTime();
+
         msl4.push("Artem", 20);
         msl4.push("Boris", 30);
 
@@ -70,7 +70,21 @@ public class HW4 {
         while (!msl4.isEmpty()){
             System.out.println("Элемент: " + msl4.pop() + " удален из списка");
         }
-        System.out.println("Task 4.5 nanoTime: " + (System.nanoTime() - t5) + "\n-------------");
+        System.out.println("Task 4.5 MyStackList nanoTime: " + (System.nanoTime() - t5_1) + "\n-------------");
+
+        MyQueue mq = new MyQueue();
+
+        long t5_2 = System.nanoTime();
+
+        mq.insert("Ivanov", 20);
+        mq.insert("Petrov", 30);
+
+        mq.display();
+
+        while (!mq.isEmpty()){
+            System.out.println("Элемент: " + mq.delete() + " удален из списка");
+        }
+        System.out.println("Task 4.5 MyQueue nanoTime: " + (System.nanoTime() - t5_2) + "\n-------------");
     }
 }
 
@@ -89,10 +103,10 @@ class MyLink4 {
     }
 }
 
-class MyLinkedList4{
+class MyLinkedList4forStack {
     public MyLink4 first;
 
-    public MyLinkedList4(){
+    public MyLinkedList4forStack(){
         first = null;
     }
 
@@ -122,10 +136,10 @@ class MyLinkedList4{
 }
 
 class MyStackList4 {
-    private MyLinkedList4 myList;
+    private MyLinkedList4forStack myList;
 
     public MyStackList4(){
-        myList = new MyLinkedList4();
+        myList = new MyLinkedList4forStack();
     }
 
     public void push(String name, int age){
@@ -142,6 +156,71 @@ class MyStackList4 {
 
     public void display(){
         myList.display();
+    }
+}
+
+class MyLinkedList4forQueue{
+    public MyLink4 first;
+    public MyLink4 last;
+
+    public MyLinkedList4forQueue(){
+        first = null;
+        last = null;
+    }
+
+    public boolean isEmpty(){
+        return (first == null);
+    }
+
+    public void insert(String name, int age){
+        MyLink4 newLink = new MyLink4(name, age);
+        if (this.isEmpty()){
+            first = newLink;
+        }else {
+            last.next = newLink;
+        }
+        last = newLink;
+    }
+
+    public String delete(){
+        MyLink4 temp = first;
+        if (first.next == null){
+            last = null;
+        }
+        first = first.next;
+        return temp.name;
+    }
+
+    public void display(){
+        MyLink4 current = first;
+        while (current != null){
+            current.display();
+            current = current.next;
+        }
+    }
+}
+
+class MyQueue{
+    private MyLinkedList4forQueue queue;
+
+    public MyQueue(){
+        queue = new MyLinkedList4forQueue();
+    }
+
+    public boolean isEmpty(){
+        return queue.isEmpty();
+    }
+
+    public void insert(String name, int age){
+        queue.insert(name, age);
+    }
+
+    public void display(){
+        queue.display();
+    }
+
+    public String delete(){
+        return queue.delete();
     }
 }
 
