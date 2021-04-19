33e8f690334b00916f9ab05895d08ac892937376
kosykt <kosvasl09@gmail.com>
2021-04-19 19:39:39 +0900

    task 6.3

--- algorithmCourse/src/main/java/HW6.java
+++ algorithmCourse/src/main/java/HW6.java
@@ -8,7 +8,13 @@ public class HW6 {
                 "\t- Большинство файловых систем.\n");
 
         //Task 6.2
+        Tree tree = new Tree();
 
+        //Task 6.3
+        tree.insert(new Person6("Ivan", 22, 01));
+        tree.insert(new Person6("Igor", 33, 02));
+        tree.insert(new Person6("Vladimir", 44, 03));
+        tree.insert(new Person6("Viktor", 55, 04));
     }
 }
 
@@ -38,15 +44,49 @@ class Tree{
     private Node root;
 
     public void insert(Person6 person6){
-
+        Node node = new Node();
+        node.person6 = person6;
+        if (root == null){
+            root = node;
+        } else {
+            Node current = root;
+            Node parent;
+            while (true){
+                parent = current;
+                if (person6.id < current.person6.id){
+                    current = current.left;
+                    if (current == null){
+                        parent.left = node;
+                        return;
+                    }
+                } else {
+                    current = current.right;
+                    if (current == null){
+                        parent.right = node;
+                        return;
+                    }
+                }
+            }
+        }
     }
 
     public Node find (int key){
-
+        Node current = root;
+        while (current.person6.id != key){
+            if (key < current.person6.id){
+                current = current.left;
+            }else {
+                current = current.right;
+            }
+            if (current == null){
+                return null;
+            }
+        }
+        return current;
     }
 
     public  boolean delete (int id){
-
+    return true;
     }
 
     public void displayTree(){
